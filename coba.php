8c40a5dc73ecebde323361a9edc3c722c2f5fc32
TegarAdityaS <mohammadtegaradityasaputra@gmail.com>
2023-08-30 22:11:05 +0700

    Utama

---
+++ coba.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Biodata</title>
+</head>
+<body>
+
+<h1>Biodata Saya</h1>
+
+<?php
+// Data biodata
+$nama = "Mohammad tegar Aditya Saputra";
+$umur = 19;
+$NIM = "E41220330";
+$Jurusan = "TI";
+
+$angka1 = 1;
+$angka2 = 1;
+$hasil = $angka1 + $angka2;
+
+// Menampilkan data biodata
+echo "<p>Nama: $nama</p>";
+echo "<p>Umur: $umur tahun</p>";
+echo "<p>Nim: $NIM</p>";
+echo "<p>Jurusan: $Jurusan</p>";
+echo "$angka1 + $angka2 = $hasil";
+?>
+
+</body>
+</html>
